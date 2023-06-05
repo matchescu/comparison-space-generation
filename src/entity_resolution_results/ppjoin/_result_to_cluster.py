80cc60f2fd7389f13eaf82ab422ad4b902a47fdd
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-06-05 09:51:18 +0300

    Implement custom exclusion lists for clustering ppjoin results

--- src/entity_resolution_results/ppjoin/_result_to_cluster.py
+++ src/entity_resolution_results/ppjoin/_result_to_cluster.py
@@ -5,11 +5,12 @@ def _exclude_columns(row: tuple, excluded: set[int]) -> tuple:
     return tuple(item for index, item in enumerate(row) if index not in excluded)
 
 
-def ppjoin_result_to_cluster(results: set[tuple[tuple, tuple, float]]) -> Clustering:
+def ppjoin_result_to_cluster(results: set[tuple[tuple, tuple, float]], exclude: list[int] = None) -> Clustering:
+    exclude = exclude or [0]
     result = []
     for cluster_a, cluster_b, _ in results:
-        x = _exclude_columns(cluster_a, {0})
-        y = _exclude_columns(cluster_b, {0})
+        x = _exclude_columns(cluster_a, set(exclude))
+        y = _exclude_columns(cluster_b, set(exclude))
         result.append((x, y))
     return Clustering(
         feature_info=[],
--- tests/entity_resolution_results/ppjoin/test_result_to_cluster.py
+++ tests/entity_resolution_results/ppjoin/test_result_to_cluster.py
@@ -8,11 +8,21 @@ def test_cluster_from_empty_results():
     assert len(clustering.feature_info) == 0
 
 
-def test_cluster_from_single_result_without_feature_info():
+def test_basic_clustering_from_single_result():
     clustering = transform_result({((0, "a", "b"), (1, "a", "c"), 0.25)})
 
     assert len(clustering.feature_info) == 0
     assert len(clustering.clustered_rows) == 1
     assert clustering.clustered_rows == [
         (("a", "b"), ("a", "c"))
+    ]
+
+
+def test_basic_clustering_from_single_result_with_custom_exclusion_list():
+    clustering = transform_result({((0, "a", "b"), (1, "a", "c"), 0.25)}, exclude=[1])
+
+    assert len(clustering.feature_info) == 0
+    assert len(clustering.clustered_rows) == 1
+    assert clustering.clustered_rows == [
+        ((0, "b"), (1, "c"))
     ]
