5a21906d8d874f9f5fc49fed8d420547f5ab9e44
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-06-10 20:25:53 +0300

    Fix code after using lists all over in abstractions module

--- src/entity_resolution_results/ppjoin/_result_to_cluster.py
+++ src/entity_resolution_results/ppjoin/_result_to_cluster.py
@@ -1,11 +1,11 @@
 from abstractions.data_structures import Clustering, FeatureInfo
 
 
-def _exclude_columns(row: tuple, excluded: set[int]) -> tuple:
-    return tuple(item for index, item in enumerate(row) if index not in excluded)
+def _exclude_columns(row: list, excluded: set[int]) -> list:
+    return [item for index, item in enumerate(row) if index not in excluded]
 
 
-def _normalize_feature_info(info: list[tuple]) -> list[tuple]:
+def _normalize_feature_info(info: list[list]) -> list[list]:
     result = []
     if info is None:
         return result
@@ -16,22 +16,18 @@ def _normalize_feature_info(info: list[tuple]) -> list[tuple]:
             if not isinstance(feature, FeatureInfo):
                 added_feature = FeatureInfo(name=str(feature), ordinal=idx)
             added_cluster.append(added_feature)
-        result.append(tuple(added_cluster))
+        result.append(added_cluster)
     return result
 
 
 def ppjoin_result_to_cluster(
-    results: set[tuple[tuple, tuple, float]],
-    feature_info: list[tuple] = None,
-    exclude: list[int] = None
+    results: list[tuple[list, list, float]],
+    feature_info: list[list] = None,
 ) -> Clustering:
-    exclude = exclude or [0]
-    result = []
-    for cluster_a, cluster_b, _ in results:
-        x = _exclude_columns(cluster_a, set(exclude))
-        y = _exclude_columns(cluster_b, set(exclude))
-        result.append((x, y))
     return Clustering(
         feature_info=_normalize_feature_info(feature_info),
-        clustered_rows=result,
+        clustered_rows=[
+            [cluster_a, cluster_b]
+            for cluster_a, cluster_b, _ in results
+        ],
     )
--- tests/entity_resolution_results/ppjoin/test_result_to_cluster.py
+++ tests/entity_resolution_results/ppjoin/test_result_to_cluster.py
@@ -2,36 +2,26 @@ from entity_resolution_results.ppjoin import transform_result
 
 
 def test_cluster_from_empty_results():
-    clustering = transform_result(set())
+    clustering = transform_result([])
 
     assert len(clustering.clustered_rows) == 0
     assert len(clustering.feature_info) == 0
 
 
 def test_basic_clustering_from_single_result():
-    clustering = transform_result({((0, "a", "b"), (1, "a", "c"), 0.25)})
+    clustering = transform_result([(["a", "b"], ["a", "c"], 0.25)])
 
     assert len(clustering.feature_info) == 0
     assert len(clustering.clustered_rows) == 1
     assert clustering.clustered_rows == [
-        (("a", "b"), ("a", "c"))
-    ]
-
-
-def test_basic_clustering_from_single_result_with_custom_exclusion_list():
-    clustering = transform_result({((0, "a", "b"), (1, "a", "c"), 0.25)}, exclude=[1])
-
-    assert len(clustering.feature_info) == 0
-    assert len(clustering.clustered_rows) == 1
-    assert clustering.clustered_rows == [
-        ((0, "b"), (1, "c"))
+        [["a", "b"], ["a", "c"]]
     ]
 
 
 def test_basic_clustering_from_single_result_with_custom_feature_info():
     clustering = transform_result(
-        {((0, "a", "b"), (1, "a", "c"), 0.25)},
-        feature_info=[("id", "value"), ("id", "size")]
+        [(["a", "b"], ["a", "c"], 0.25)],
+        feature_info=[["id", "value"], ["id", "size"]]
     )
 
     assert len(clustering.feature_info) == 2
@@ -42,5 +32,5 @@ def test_basic_clustering_from_single_result_with_custom_feature_info():
     assert actual_feature_info == [("id", "value"), ("id", "size")]
     assert len(clustering.clustered_rows) == 1
     assert clustering.clustered_rows == [
-        (("a", "b"), ("a", "c"))
+        [["a", "b"], ["a", "c"]]
     ]
