2ea65a42b875d5e65193e4c4f6bf1872f443ccfe
Andrei Olar <andrei.olar@gmail.com>
2025-03-21 21:55:56 +0200

    make this repo about comparison-spage-generation

--- src/entity_resolution_results/ppjoin/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._result_to_cluster import ppjoin_result_to_cluster as transform_result
-
-
-__all__ = ["transform_result"]
--- src/entity_resolution_results/ppjoin/_result_to_cluster.py
+++
@@ -1,9 +0,0 @@
-from abstractions.data_structures import Clustering
-
-
-def ppjoin_result_to_cluster(results: list[tuple[list, list, float]]) -> Clustering:
-    return Clustering.from_nested_lists([
-            [cluster_a, cluster_b]
-            for cluster_a, cluster_b, _ in results
-        ],
-    )
