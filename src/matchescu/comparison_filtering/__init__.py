aabab3c1d7cc4f4004950faad1591172f4dca3f9
Andrei Olar <andrei.olar@gmail.com>
2025-03-22 12:51:35 +0200

    Extract blocking and filtering from the matching repo
    Comparison Space generation is the activity of determining the comparisons that an entity resolution must make.
    Blocking and comparison filtering are two essential activities in this process.

---
+++ src/matchescu/comparison_filtering/__init__.py
@@ -0,0 +1,8 @@
+from matchescu.comparison_filtering._filters import (
+    ComparisonFilter,
+    MultiSourceFilter,
+    JaccardSimilarityFilter,
+)
+
+
+__all__ = ["ComparisonFilter", "MultiSourceFilter", "JaccardSimilarityFilter"]
