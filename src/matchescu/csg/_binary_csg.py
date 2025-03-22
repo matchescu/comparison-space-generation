3c09201077d07e192076238761459e853dc0e8df
Andrei Olar <andrei.olar@gmail.com>
2025-03-22 13:17:07 +0200

    ensure comparison space generator calls blocker

--- src/matchescu/csg/_binary_csg.py
+++ src/matchescu/csg/_binary_csg.py
@@ -1,8 +1,12 @@
 from matchescu.blocking import Blocker
 from matchescu.comparison_filtering import ComparisonFilter
+from matchescu.reference_store.comparison_space import BinaryComparisonSpace
 
 
 class BinaryComparisonSpaceGenerator:
     def __init__(self):
         self._blockers: list[Blocker] = []
         self._filters: list[ComparisonFilter] = []
+
+    def __call__(self) -> BinaryComparisonSpace:
+        pass
